2daea67c1bc57d563cc58a853231cfa8e011da73
lucianodiaz <lucianodiazberberich@gmail.com>
2020-04-07 16:43:55 -0300

    Modificacion de todo
    Funciona capo

---
+++ Micrositio_Acreditacion/Models/DetallesSolicitudesMuni.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Micrositio_Acreditacion.Models
+{
+    public class DetallesSolicitudesMuni
+    {
+        public string dni { get; set; }
+        public string nombre { get; set; }
+        public string importe { get; set; }
+        public string celular { get; set; }
+        public string compcel { get; set; }
+        public string email { get; set; }
+    }
+}
